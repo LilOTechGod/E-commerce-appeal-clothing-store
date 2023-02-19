e6d6a394fe1a31ffc2b25464d6c8541842e9fbf5
unknown <perezoscar360@gmail.com>
2023-02-19 17:52:41 -0600

    I've added the react meta tags npm package to the server and source folder inorder to add meta tags inside components, installed Material ui inside source folder for styling purposes, and started on the nav bar for the launch page

--- arcadia-appeal-clothing-store/src/components/launchPage/launchPage.js
+++ arcadia-appeal-clothing-store/src/components/launchPage/launchPage.js
@@ -1,15 +1,43 @@
 import React from 'react'
+import MetaTags from 'react-meta-tags'
+import {AppBar, Toolbar, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 
 const launchPage = () => {
   return (
-    <div>
+    <div className='App'>
+       <MetaTags>
+            <title>Arcadia start page</title>
+            <meta name="description" content="Some description." />
+            <meta property="og:title" content="MyApp" />
+            <meta property="og:image" content="" />
+            <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </MetaTags>
       <header>
-        <h1>Welcome: (username here)</h1>
-        <h1>Log In / Logout</h1>
+        <Box sx= {{flexGrow: 1, marginBottom: 3}}>
+          <AppBar position='static'>
+            <Toolbar>
+              <Typography
+                noWrap
+                component="div"
+                sx={{mr: 2, display: {xs: "none", md: "flex"}}}
+                >
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHStElGWMbiyq8Qn7HtR7f2Nhpu9mmXyTBSw&usqp=CAU" style={{width: 172, height: 140}}/>
+                </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
       </header>
-      
+      <main>
+        <h1>Arcadia</h1>
+        <div className='carosel'></div>
+        <h2>Categories</h2>
+        <div className='categories'>
+
+        </div>
+      </main>
     </div>
-  )
+  );
 }
 
 export default launchPage
--- arcadia-appeal-clothing-store/src/components/logIn/logIn.js
+++ arcadia-appeal-clothing-store/src/components/logIn/logIn.js
@@ -3,7 +3,7 @@ import React from 'react'
 const logIn = () => {
   return (
     <div>
-      <p>hello world login page</p>
+      
     </div>
   )
 }
