3b17798f6061706154656a5b99f69362972ce885
unknown <perezoscar360@gmail.com>
2023-02-21 22:24:23 -0600

    working on getting the source code from material.ui.com docs to render the working components

--- arcadia-appeal-clothing-store/src/App.js
+++ arcadia-appeal-clothing-store/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import LaunchPage from './components/launchPage/launchPage';
 import LogIn from './components/logIn/logIn';
+import Appbar from './components/Appbar/app';
 
 function App() {
   return (
     <div>
       <LaunchPage />
       <LogIn />
+      <Appbar />
     </div>
   );
 }
