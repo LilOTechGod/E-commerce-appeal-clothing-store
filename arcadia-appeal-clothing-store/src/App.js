7a80d3ecc5e4e2ae63f4bb1328c206fe8d5f7324
unknown <perezoscar360@gmail.com>
2023-03-05 17:02:50 -0600

    started on the carousel for the launch page, I now have to add the subtitle and catergory cards, then style the page and edit a couple things in order to complete the launch page

--- arcadia-appeal-clothing-store/src/App.js
+++ arcadia-appeal-clothing-store/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import LaunchPage from './components/launchPage/launchPage';
 import Appbar from './components/Appbar/app';
+import Carousel from './components/Carousel/carousel'
 
 function App() {
   return (
     <div>
       <LaunchPage />
       <Appbar />
+      <Carousel />
     </div>
   );
 }
