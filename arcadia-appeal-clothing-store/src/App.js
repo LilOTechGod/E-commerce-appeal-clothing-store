5a3615908daa87319732b81c597cbf88ead33673
ChiemekaAnunkor <anunkorjr@gmail.com>
2023-03-12 05:20:12 -0500

    added images to display

--- arcadia-appeal-clothing-store/src/App.js
+++ arcadia-appeal-clothing-store/src/App.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 import LaunchPage from './components/launchPage/launchPage';
 import Appbar from './components/Appbar/app';
 import Carousel from './components/Carousel/carousel'
+
+import Products from "./components/products/Products"
 
 function App() {
   return (
     <div>
       <LaunchPage />
       <Appbar />
-      <Carousel />
+
+
+      <Router>
+
+        <Routes>
+
+          <Route path='/' element={<Carousel />} />
+          <Route path='/products' element={<Products />} />
+
+
+        </Routes>
+      </Router>
+
     </div>
   );
 }
-
 export default App;
